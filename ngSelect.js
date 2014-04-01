b41103deec2946456964c1779b28b5f327a3fb53
SW4 <samuel.wheeler@gmail.com>
2014-04-01 15:30:47 +0100

    update 0.0.1

--- ngSelect.js
+++ ngSelect.js
@@ -1,15 +1,15 @@
-
 $( document ).ready(function() {
     $('html').on('click',function(e){
-        if($(e.target).parents('div.ngSelect').length>0){
-            $(e.target).parents('div.ngSelect').find('ul ul').toggleClass('active');
+        var ngSelect=$(e.target).parents('div.ngSelect');
+        if(ngSelect.length>0){
+           !ngSelect.children('select').is(':disabled') && ngSelect.find('ul ul').toggleClass('active');
         }
-        $('div.ngSelect ul ul').not($(e.target).parents('div.ngSelect').find('ul ul')).removeClass('active');
+        $('div.ngSelect ul ul').not(ngSelect.find('ul ul')).removeClass('active');
         
     });
     $('select.ngSelect').each(function(){
         var select=$(this);
-        var el=select.get(0);
+        var el=select.get(0);        
         var scope = angular.element(el).scope();
         var model=select.data("ng-model") || select.attr("ng-model");
         var wrapper=$("<div class='ngSelect'></div>");
@@ -18,18 +18,19 @@ $( document ).ready(function() {
            return select.children('option').length;
         }, function() {    
             var ngChoices=$("<li></li>");
+            var ngSelection=$("<span></span>");
             var ngOptions=$("<ul></ul>");
             ngSelect.html('');
             select.children('option').each(function(index,o){
                 var option=$(o);
-                index==0 && ngChoices.append("<span>"+option.text()+"</span>");
+                index==0 && ngSelection.text(option.text()) && ngChoices.append(ngSelection);
                 var ngOption=$("<li>"+option.text()+"</li>");
                 ngOption.on('click', function(){
                     var selected=$(this);
+                    ngSelection.text(selected.text());
                     scope.$apply(function(){
                         scope[model]=selected.text();
                     });
-                    ngSelect.find('span').text(selected.text());
                 });
                 ngOptions.append(ngOption)
             });
