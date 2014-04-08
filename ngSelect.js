c2123c843e650f635fb06900dc847e9331e9c92d
SW4 <samuel.wheeler@gmail.com>
2014-04-08 13:06:26 +0100

    update 0.0.3

--- ngSelect.js
+++ ngSelect.js
@@ -2,45 +2,42 @@ $( document ).ready(function() {
     $('html').on('click',function(e){
         var ngSelect=$(e.target).parents('div.ngSelect');
         if(ngSelect.length>0){
-           !ngSelect.children('select').is(':disabled') && ngSelect.find('ul ul').toggleClass('active');
+           !ngSelect.children('select').is(':disabled') && ngSelect.toggleClass('active');
         }
-        $('div.ngSelect ul ul').not(ngSelect.find('ul ul')).removeClass('active');
-        
+        $('div.ngSelect').not(ngSelect).removeClass('active');        
     });
-    $('select.ngSelect').each(function(){
-        var select=$(this);
-        var el=select.get(0);        
-        var scope = angular.element(el).scope();
-        var model=select.data("ng-model") || select.attr("ng-model");
-        var wrapper=$("<div class='ngSelect'></div>");
-        var ngSelect=$("<ul></ul>");        
+    $('select.ngSelect').each(function(){    	
+        var select=$(this),
+        	el=select.get(0),
+        	scope = angular.element(el).scope(),
+        	model=select.data("ng-model") || select.attr("ng-model"),
+        	wrapper=$("<div class='ngSelect'></div>"),
+        	loadingString="Loading...",
+        	ngSelected=$("<div>"+loadingString+"</div>"),
+        	ngSelections=$("<ul></ul>");        
         scope.$watch(function () {
            return select.children('option').length;
-        }, function() {    
-            var ngChoices=$("<li></li>");
-            var ngSelection=$("<span></span>");
-            var ngOptions=$("<ul></ul>");
-            ngSelect.html('');
+        }, function() { 
+        	ngSelections.html('');
             select.children('option').each(function(index,o){
                 var option=$(o);
-                index==0 && ngSelection.text(option.text()) && ngChoices.append(ngSelection);
+                if(option.text()==''){
+                	ngSelected.text(loadingString);
+                	return false;
+                }
+                index==0 && ngSelected.text(option.text());
                 var ngOption=$("<li>"+option.text()+"</li>");
                 ngOption.on('click', function(){
                     var selected=$(this);
-                    ngSelection.text(selected.text());
+                    ngSelected.text(selected.text());
                     scope.$apply(function(){
                         scope[model]=selected.text();
                     });
                 });
-                ngOptions.append(ngOption)
+                ngSelections.append(ngOption);
             });
-            ngSelect.append(ngChoices.append(ngOptions));
-        });
-        wrapper.css({
-            height:select.outerHeight(),
-            width:select.outerWidth()
-        });
-        select.hide();
-        select.wrap(wrapper).parent().append(ngSelect); 
+            select.parent().append(ngSelections);
+        });      
+        select.hide().wrap(wrapper).parent().append(ngSelected); 
     });
 });
